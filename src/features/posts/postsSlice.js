a78cf27f6385df2ad40edecafa4994fdc35ee074
caotrananhk <caotrananhk@gmail.com>
2023-09-04 23:35:56 +0700

    update

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,16 +1,43 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { nanoid } from '@reduxjs/toolkit'
+import { sub } from 'date-fns'
 
 const initialState = [
-  { id: '1', title: 'First Post!', content: 'Hello!' },
-  { id: '2', title: 'Second Post', content: 'More text' },
+  {
+    id: '1',
+    title: 'First Post!',
+    content: 'Hello!',
+    user: '0',
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
+  },
+  {
+    id: '2',
+    title: 'Second Post',
+    content: 'More text',
+    user: '1',
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
+  },
 ]
 
 const postsSlice = createSlice({
   name: 'posts',
   initialState,
   reducers: {
-    postAdded(state, action) {
-      state.push(action.payload)
+    postAdded: {
+      reducer(state, action) {
+        state.push(action.payload)
+      },
+      prepare(title, content, userId) {
+        return {
+          payload: {
+            id: nanoid(),
+            title,
+            content,
+            user: userId,
+            date: new Date().toISOString(),
+          },
+        }
+      },
     },
     postUpdated(state, action) {
       const { id, title, content } = action.payload
